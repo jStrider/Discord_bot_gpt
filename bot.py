669aac5549629bec810c2172f169a6e03a3a1b41
jStrider <ju.renaud974@gmail.com>
2023-03-28 09:45:44 +0200

    fixed bot and added gpt support

--- bot.py
+++ bot.py
@@ -1,41 +1,52 @@
 import os
 import discord
+from discord.ext import commands
+import openai
 from dotenv import load_dotenv
 from discord import Intents
+
+
+
 
 # Charger les variables d'environnement
 load_dotenv()
 TOKEN = os.getenv("DISCORD_TOKEN")
 specific_channel_id = int(os.getenv("CHANNEL"))
+#OPENAI
+openai.organization = os.getenv("OPENAI_ORGID")
+openai.api_key = os.getenv("OPENAI_TOKEN")
+init_prompt = os.getenv("INIT_PROMPT")
+
 
 # Créer un objet Intents avec les intents par défaut
 intents = Intents.default()
 intents.message_content=True
 
-# Initialiser le client Discord
-client = discord.Client(intents=intents)
 
-# Définir l'événement pour les messages reçus
-@client.event
-async def on_message(message):
-    # Ignorer les messages du bot lui-même
-    if message.author == client.user:
-        return
+# Initialize the bot with a command prefix
+bot = commands.Bot(command_prefix="!",intents=intents)
 
-    print(f"Message reçu de {message.author} dans le canal {message.channel.id}: {message.content}")  # Ajouter un message de débogage
+# Define the 'hello' command
+@bot.command()
+async def pickup(ctx, *, text: str):
 
-    # Répondre dans le canal spécifique
-    if message.channel.id == specific_channel_id:
-        print("bon message dans le bon channel")
-        await message.channel.send("Bonjour !")
-        print(message.content)
-        if message.content.lower().startswith("bonjour"):  # Modification de la condition
-            await message.channel.send("Bonjour !")
+    if ctx.channel.id == specific_channel_id:
+        gpt_result = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        max_tokens=150,
+        messages=[
+            {"role" : "system", "content":init_prompt},
+            {"role" : "user", "content" : text}
+        ] 
 
-# Afficher un message lorsque le bot est prêt
-@client.event
+    )
+
+    await ctx.channel.send(gpt_result.choices[0].message.content.strip())
+  
+# # Afficher un message lorsque le bot est prêt
+@bot.event
 async def on_ready():
-    print(f"{client.user} est connecté à Discord !")
+    print(f"{bot.user} est connecté à Discord !")
 
 # Démarrer le bot
-client.run(TOKEN)
+bot.run(TOKEN)
